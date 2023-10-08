078925092783c94fc2f58704554ea4aa169f872a
KobeLemon <kobe42lemon@outlook.com>
2023-10-08 03:18:07 -0600

    Added SWAPI link & BackToTop Btn

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,7 @@
 // find the element with the id of "copyrightYear" & update the innerHTML element to show the current year dynamically
-document.getElementById("copyrightYear").innerHTML = `${new Date().getFullYear()} | Kobe Lemon`;
+document.querySelector("#copyrightYear").innerHTML = `${new Date().getFullYear()} | Kobe Lemon`;
 
 // find the element with the id of "date" & update the innerHTML to show the document's last modified date/time
-document.getElementById("lastModified").innerHTML = document.lastModified;
+document.querySelector("#lastModified").innerHTML = document.lastModified;
+
+document.querySelector('#backToTopBtn').addEventListener('click', () => {document.querySelector('header').scrollIntoView({behavior: "smooth"})});
